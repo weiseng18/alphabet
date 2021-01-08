40e9b1ff1c192229429b61b750be932ccabd4794
weiseng18 <tws.hci@gmail.com>
2021-01-08 16:47:16 +0800

    [js] create init_resources function to initialize table to display resources from Game object

--- js/main.js
+++ js/main.js
@@ -6,10 +6,25 @@ function Game() {
 		money: 0
 	};
 }
+
+Game.prototype.init_resources = function() {
+	let table = get("resources");
+	for (const resource in this.resources) {
+		let row = table.insertRow();
+		// resource
+		let resourceType = row.insertCell();
+		resourceType.innerHTML = resource;
+		// amount
+		let amount = row.insertCell();
+		amount.innerHTML = this.resources[resource];
+	}
+}
+
 var game;
 
 function init() {
 	game = new Game();
+	game.init_resources();
 }
 
 window.addEventListener ? window.addEventListener("load",init,false) : window.attachEvent && window.attachEvent("onload", init);
