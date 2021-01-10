41b1d3661eee632052e84ab3a4ec3bca8dccb5cd
weiseng18 <tws.hci@gmail.com>
2021-01-10 22:44:43 +0800

    [js] implement trigger_wordAnimation(), to spawn a word on the printer, which floats up and fades away

--- js/main.js
+++ js/main.js
@@ -170,6 +170,107 @@ Upgrade.prototype.formula = function() {
 		return this.baseAmount * (this.baseMultiplier)**(this.level-1);
 }
 
+function wordAnimation(singleDistance, singleOpacity) {
+	/*
+		Description:
+		this function is a single pass for the animation on the word
+
+		Parameters:
+		singleDistance (in px)
+		singleOpacity
+	*/
+
+	// update opacity
+	let opacity = this.style.opacity;
+	let newOpacity = opacity - singleOpacity;
+	this.style.opacity = newOpacity;
+
+	// update y
+	let y = parseFloat(this.style.top);
+	let newY = y - singleDistance;
+	this.style.top = newY + "px";
+
+}
+
+function trigger_wordAnimation(word) {
+	/*
+		Description:
+		this function will create a word element, and then set up the following animation for a specific word:
+			- word spawns on #printerArt
+			- word floats up and fades away and disappears
+		wordAnimation() contains a single pass for the animation on the word
+	*/
+
+	// step 1: calculate center location to spawn the word
+	let rect = get("printerArt").getBoundingClientRect();
+
+	let middleLocation = {
+		x: (rect.left + rect.right) / 2,
+		y: (rect.top + rect.bottom) / 2
+	};
+
+	// step 2: create floater element
+
+	// step 2.1: HTML element
+
+		// step 2.1.1: basic HTML element details
+		let floater = document.createElement("span");
+		floater.innerHTML = word;
+
+		// step 2.1.2: set basic style
+		floater.style.position = "absolute";
+		floater.style.opacity = 1;
+		floater.style.zIndex = 100;
+
+	// step 2.2: calculate location
+
+		// step 2.2.1: inject into DOM to use .getBoundingClientRect()
+		floater.style.visibility = "hidden";
+		document.body.appendChild(floater);
+		let dimensions = floater.getBoundingClientRect();
+		document.body.removeChild(floater);
+		floater.style.visibility = "visible";
+
+		// step 2.2.2: calculate and set location
+		let spawnLocation = {
+			x: middleLocation.x - dimensions.width / 2,
+			y: middleLocation.y - dimensions.height / 2
+		};
+
+		floater.style.top = spawnLocation.y + "px";
+		floater.style.left = spawnLocation.x + "px";
+
+	// step 3: animation
+
+		// step 3.1: define constants
+
+		// note: duration, updateRate (in milliseconds)
+		let duration = 1000;
+		let updateRate = 10;
+		// note: distance (in px)
+		let distance = 100;
+
+		// divide duration by updateRate, to get the iterations required
+		let iterations = duration / updateRate;
+
+		// calculate change required in one pass
+		let singleDistance = distance / iterations;
+		let singleOpacity = 1.0 / iterations;
+
+		// step 3.2: attach setInterval and setTimeout
+
+		var interval = setInterval( wordAnimation.bind(floater, singleDistance, singleOpacity), updateRate);
+
+		// delete element after all passes complete
+		setTimeout( () => {
+			floater.parentNode.removeChild(floater);
+			clearInterval(interval);
+		}, duration);
+
+	// step 4: append to body
+	document.body.appendChild(floater);
+}
+
 function add_printerRefillButtons() {
 	/*
 		Description:
