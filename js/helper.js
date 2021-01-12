dd39c84e07be3eced0137b9cf03fc47bb8a534d1
weiseng18 <tws.hci@gmail.com>
2021-01-12 14:22:42 +0800

    [helper] add getCurrentTime to return time in HH:MM:SS format

--- js/helper.js
+++ js/helper.js
@@ -1,3 +1,13 @@
 function get(id) {
 	return document.getElementById(id);
 }
+
+function getCurrentTime() {
+	/*
+		Description:
+		Gets time in the format HH:MM:SS
+	*/
+	let d = new Date();
+	let formatted = d.toTimeString().split(" ")[0];
+	return formatted;
+}
