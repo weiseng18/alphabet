52e16767fce6c60f8a1830de9805c3565f66610c
weiseng18 <tws.hci@gmail.com>
2021-01-12 16:20:11 +0800

    [js] add notifications.js for Notification object, that adds notifications to #notifications div, given necessary parameters

---
+++ js/notifications.js
@@ -0,0 +1,24 @@
+function Notification(type, data) {
+	this.type = type;
+	this.data = data;
+	this.addNotification();
+}
+
+Notification.prototype.addNotification = function() {
+	// build innerHTML
+	let time = getCurrentTime();
+	let innerHTML;
+	if (this.type == "word" || this.type == "letter")
+		innerHTML = "You discovered a new " + this.type + ": <strong>" + this.data + "</strong>";
+
+	// wrapper div element
+	let div = document.createElement("div");
+	div.innerHTML = innerHTML;
+	// div.style.opacity = 0;
+
+	// fade-in animation
+	// can reference trigger_wordAnimation() when implementing this
+
+	// insert after <h2>
+	get("notifications").insertBefore(div, get("notifications").children[0].nextSibling);
+}
