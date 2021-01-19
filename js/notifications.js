c28ba7a4fa797b3bb04140357e36e96b6cd8e9e1
weiseng18 <tws.hci@gmail.com>
2021-01-19 16:11:07 +0800

    [js] add time to notifications

--- js/notifications.js
+++ js/notifications.js
@@ -6,12 +6,12 @@ function Notification(type, data) {
 
 Notification.prototype.addNotification = function() {
 	// build innerHTML
-	let time = getCurrentTime();
+	let time = "[" + getCurrentTime() + "]";
 	let innerHTML;
 	if (this.type == "word" || this.type == "letter")
-		innerHTML = "You discovered a new " + this.type + ": <strong>" + this.data + "</strong>";
+		innerHTML = time + " You discovered a new " + this.type + ": <strong>" + this.data + "</strong>";
 	else if (this.type == "insufficient")
-		innerHTML = "You have insufficient <strong>" + this.data + "</strong>";
+		innerHTML = time + " You have insufficient <strong>" + this.data + "</strong>";
 
 	// wrapper div element
 	let div = document.createElement("div");
